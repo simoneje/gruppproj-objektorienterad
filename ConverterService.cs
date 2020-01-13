8c7d3227fb0f1a08c32359f2f50e5bb0264f3d60
simoneje <58808601+simoneje@users.noreply.github.com>
2020-01-13 10:04:41 +0100

    Add files via upload

---
+++ ConverterService.cs
@@ -0,0 +1,87 @@
+﻿
+using System;
+namespace Service
+{
+    public class ConverterService : IConverterService
+    {
+        public ConverterService()
+        {
+        }
+
+        // 1 gallon = 3.78541178 liters     
+        // 1 liter = 0.264172052 gallons   
+        //Fahrenheit = Celsius * 9/5 + 32
+        //Time = distance / speed
+        //Ohm law says: amp = voltage / resistance
+        //Radius = square root (area / pi)
+        //Diameter = radius * 2
+
+        public float CalculateAmps(float voltage, float resistance)
+        {
+            float amps = voltage/resistance;
+            return amps;
+             
+        }
+
+        public float CalculateResistance(float voltage, float amps)
+        {
+            return voltage/amps;
+        }
+
+        public float CalculateDiameterFromArea(float area)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float CalculateRadiusFromArea(float area)
+        {
+            throw new NotImplementedException();
+        }
+        public float CalculateTravelTime(float speed, float distance)
+        {
+            throw new NotImplementedException();
+        }
+        public float CalculateTraveledDistance(float speed, float time) {
+            throw new NotFiniteNumberException();
+        }
+        public float CalculateSpeed(float time, float distance) {
+            throw new NotFiniteNumberException();
+        }
+
+        public float CalculateVoltage(float res, float amp)
+        {
+            float voltage = amp * res;
+            return voltage;
+        }
+        
+        public float ConvertCelciusToFahrenheit(float celcius)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float ConvertFahrenheitToCelcius(float fahrenheit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float ConvertFromGallonToLiter(float gallon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float ConvertFromLiterToGallon(float liter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float ConvertFromHoursToMinutes(float hours)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float ConvertFromMinutesToHours(float minutes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
